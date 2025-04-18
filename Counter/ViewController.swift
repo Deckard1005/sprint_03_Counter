87cf0fc556d1f59caf84a4abcdc48c7efd4df57e
Larkin <larkin93@icloud.com>
2025-04-18 21:24:02 +0800

    Задание отправлено на проверку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterValue: UILabel!
+    
+    @IBOutlet weak var counterButtonPlus: UIButton!
+    
+    @IBOutlet weak var counterButtonMinus: UIButton!
+    
+    @IBOutlet weak var counterDownToZero: UIButton!
+    
+    @IBOutlet weak var history: UITextView!
+    
+    
+    var counter: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterValue.text = "Значение счётчика: \(counter)"
+        counterButtonPlus.setTitle("+", for: .normal)
+        history.isEditable = false
+        history.isScrollEnabled = true
+        history.text = "История изменений: \n"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func counterIncrease(_ sender: Any) {
+        if counter < Int.max {
+            counter += 1
+        } else {
+            counter = Int.max
+        }
+        counterValue.text = "Значение счётчика: \(counter)"
+        logHistory("значение изменено на +1")
+    }
+    
+    
+    @IBAction func counterDecrease(_ sender: Any) {
+        if counter <= 0 {
+            counter = 0
+            logHistory("попытка уменьшить значение счётчика ниже 0")
+        } else {
+            counter -= 1
+            logHistory("значение изменено на -1")
+        }
+        counterValue.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func counterDownToZero(_ sender: Any) {
+        counter = 0
+        counterValue.text = "Значение счётчика: \(counter)"
+        logHistory("значение сброшено")
+    }
+    
+    func logHistory(_ message: String) {
+        let time = Date().formatted(date: .numeric, time: .standard)
+        let entry = "\(time): \(message)\n"
+        history.text += entry
+        
+    }
+    
 }
 
